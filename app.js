23e28c0b1bb01affb37f20d0dfd139cd2c096039
evil3sc <hyp3rbone@outlook.com>
2022-10-24 00:06:00 -0600

    Firebase, messages and commands

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const {initializeApp} = require('firebase/app')
 const Pusher = require("pusher");
 const express = require('express')
 const favicon = require('serve-favicon')
@@ -11,6 +12,18 @@ const pusher = new Pusher({
   cluster: 'us2',
   useTLS: true
 });
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+const fbApp = initializeApp(firebaseConfig);
 
 app.use(express.static(__dirname+'/public'))
 app.use(favicon(__dirname + '/public/img/favicon.ico'))
--- public/js/controller.js
+++ public/js/controller.js
@@ -1,7 +1,11 @@
 const input = document.getElementById('input');
 const lines = document.getElementById('lines');
 const linesContainer = document.getElementById('lines-container');
-console.log(linesContainer)
+let loginState = false;
+let loadI = 0;
+let loadingState = false;
+
+const loadChars = ['/', '-', '\\', '|']
 
 Pusher.logToConsole = true;
 
@@ -35,13 +39,58 @@ function sendSimple(msg) {
   settleLines()
 }
 
+function addLine(carret, content) {
+  let ln = document.createElement('div')
+  ln.className = 'line'
+  let car = document.createElement('div')
+  car.className = 'line-carret'
+  car.innerText = carret
+  let cont = document.createElement('div')
+  cont.className = 'line-cont'
+  cont.innerText = content
+  ln.appendChild(car)
+  ln.appendChild(cont)
+  lines.appendChild(ln)
+  settleLines()
+}
+
+function sysLine(msg) {
+  addLine('sys>\xa0', msg)
+}
+
+function localLine(msg) {
+  addLine('>\xa0', msg)
+}
+
+function setMainCarret(carret) {
+  document.getElementById('input-carret').innerText = `${carret}\xa0`
+}
+
 input.addEventListener('keypress', (e) => {
   const keyPressed = e.key
   if (keyPressed === 'Enter') {
     e.preventDefault();
-    if(e.target.value !== '') {
-      sendSimple(e.target.value)
+    if (e.target.value[0] == '/' && e.target.value.length == 2) {
+      switch (e.target.value[1]) {
+        case 'L':
+          sysLine('Loading state')
+          setLoading(true)
+          break;
+        case 'S':
+          sysLine('Loading state off')
+          setLoading(false)
+          setMainCarret('MSG>')
+          break;
+        default:
+          sysLine('Unknown command!')
+          break;
+      }
+      e.target.value = ''
+    } else if(e.target.value !== '') {
+      localLine(e.target.value)
+      e.target.value = ''
     }
+    
   } else {
     
   }
@@ -50,10 +99,13 @@ input.addEventListener('keypress', (e) => {
 input.addEventListener('input', (e) => {
   e.target.style.height = '1rem';
   e.target.style.height = ((e.target.scrollHeight) / 16) + 'rem';
-  if(e.target.value[0] == '/') {
-    document.getElementById('input-carret').innerText = 'OPT>\xa0'
-  } else {
-    document.getElementById('input-carret').innerText = 'MSG>\xa0'
+  console.log(loadingState)
+  if(!loadingState){
+    if(e.target.value[0] == '/') {
+      setMainCarret('OPT>')
+    } else {
+      setMainCarret('MSG>')
+    }
   }
 })
 
@@ -62,3 +114,39 @@ function settleLines() {
     linesContainer.scrollTop = linesContainer.scrollHeight;
   }, 200)
 }
+
+settleLines()
+
+function setLogin() {
+  input.childNodes = new Array();
+  sysLine('Log with a cool name!')
+  addLine('', '\xa0')
+  setMainCarret('username:')
+  // TODO: Set opt and foot
+}
+
+
+function entryState() {
+  if(localStorage.getItem('profile') !== null) {
+    //load with profile
+  } else {
+    //set login phase
+    setLogin()
+  }
+}
+
+function setLoading(loading) {
+  if(loading) {
+    loadingState = true
+    rec = setTimeout(() => {
+      setMainCarret(`\xa0${loadChars[loadI%loadChars.length]}\xa0`)
+      loadI++
+      setLoading(true)
+    }, 200)
+  } else {
+    loadingState = false
+    clearTimeout(rec)
+  }
+}
+
+entryState()

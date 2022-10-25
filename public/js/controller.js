7a635c66e6f841123a1386d27a3dd084accf9e74
evil3sc <hyp3rbone@outlook.com>
2022-10-25 17:09:34 -0600

    basic, messaging, options, and logging/log off

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,13 +1,16 @@
 const input = document.getElementById('input');
 const lines = document.getElementById('lines');
 const linesContainer = document.getElementById('lines-container');
+const opts = document.getElementById('opts');
+const foot = document.getElementById('foot');
+let profile = {}
 let loginState = false;
 let loadI = 0;
-let loadingState = false;
+let loading = false;
 
 const loadChars = ['/', '-', '\\', '|']
 
-Pusher.logToConsole = true;
+//Pusher.logToConsole = true;
 
 var pusher = new Pusher('9f0b98fbf42211664194', { cluster: 'us2' });
 var channel = pusher.subscribe('chnnl');
@@ -17,13 +20,13 @@ channel.bind('event', function(data) {
 });
 
 function pushData(data) {
-  console.log(JSON.stringify(data));
+  console.log(JSON.stringify(data))
   fetch("api/event-channel", {
     method: "POST",
     headers: {'Content-Type': 'application/json'}, 
     body: JSON.stringify(data)
   }).then(res => {
-    console.log("Request complete! response:", res);
+    console.log("Request complete! response:", res)
   });
 }
 
@@ -54,11 +57,11 @@ function addLine(carret, content) {
   settleLines()
 }
 
-function sysLine(msg) {
+function addSysLine(msg) {
   addLine('sys>\xa0', msg)
 }
 
-function localLine(msg) {
+function addLocalLine(msg) {
   addLine('>\xa0', msg)
 }
 
@@ -66,41 +69,88 @@ function setMainCarret(carret) {
   document.getElementById('input-carret').innerText = `${carret}\xa0`
 }
 
+function setOptions(optText) {
+  opts.innerText = optText
+}
+
+function setFoot(ftext) {
+  foot.innerText = ftext
+}
+
+function printBanenr() {
+  
+}
+
+function logIn() {
+  if(loginState) {
+    if(input.value !== '' && input.value.length > 2 && input.value.length < 20) {
+      //login
+      loginState = false
+      localStorage.setItem('profile', JSON.stringify({'uname': input.value.trim()}))
+      input.value = ''
+      setLogged()
+    } else {
+      addSysLine('Invalid username!')
+    }
+  }
+}
+
 input.addEventListener('keypress', (e) => {
   const keyPressed = e.key
   if (keyPressed === 'Enter') {
     e.preventDefault();
     if (e.target.value[0] == '/' && e.target.value.length == 2) {
-      switch (e.target.value[1]) {
-        case 'L':
-          sysLine('Loading state')
+      switch (e.target.value[1].toLowerCase()) {
+        case 'l':
+          addSysLine('Loading state')
           setLoading(true)
           break;
-        case 'S':
-          sysLine('Loading state off')
+        case 's':
+          addSysLine('Loading state off')
           setLoading(false)
           setMainCarret('MSG>')
           break;
+        case 'c':
+          clearLines()
+          break;
+        case 'a':
+          for (let i = 0; i < 10; i++) {
+            addLocalLine('hi')
+          }
+          break;
+        case 'q':
+          clearLines()
+          localStorage.clear('profile')
+          addSysLine('Deleting profile')
+          addSysLine('Loggin off')
+          addLine('', '\xa0')
+          setLogin()
+          break;
+        case 'b':
+          addSysLine('Work on progress!')
+          break;
+        case 'i':
+          addSysLine('Información valiosísima!')
+          break;
         default:
-          sysLine('Unknown command!')
+          addSysLine('Unknown command!')
           break;
       }
       e.target.value = ''
+    } else if(loginState) {
+      logIn()
     } else if(e.target.value !== '') {
-      localLine(e.target.value)
+      addLocalLine(e.target.value)
       e.target.value = ''
     }
-    
-  } else {
-    
+    e.target.style.height = '1rem';
   }
 })
 
 input.addEventListener('input', (e) => {
   e.target.style.height = '1rem';
   e.target.style.height = ((e.target.scrollHeight) / 16) + 'rem';
-  console.log(loadingState)
-  if(!loadingState){
+  if(!loading && !loginState){
     if(e.target.value[0] == '/') {
       setMainCarret('OPT>')
     } else {
@@ -115,37 +165,59 @@ function settleLines() {
   }, 200)
 }
 
-settleLines()
-
-function setLogin() {
-  input.childNodes = new Array();
-  sysLine('Log with a cool name!')
-  addLine('', '\xa0')
-  setMainCarret('username:')
-  // TODO: Set opt and foot
+function clearLines() {
+  var child = lines.lastElementChild; 
+  while (child) {
+    lines.removeChild(child);
+      child = lines.lastElementChild;
+  }
 }
 
 
+function setLogin() {
+  loginState = true
+  addSysLine('Log with a cool name!')
+  addLine('', '\xa0')
+  setMainCarret('username:')
+  setFoot('\xa0')
+}
+
+function setLogged() {
+  profile = JSON.parse(localStorage.getItem('profile'))
+  clearLines()
+  opts.classList.remove('hidden')
+  setOptions('Q:Quit\xa0\xa0B:Load Board\xa0\xa0I:Info')
+  setMainCarret('MSG>')
+  addLocalLine(`Logged as ${profile.uname}` )
+  addLine('', '\xa0')
+  addSysLine(`Welcome ${profile.uname}!`)
+  addSysLine('Write te command of your choose. /h for help.')
+  addLine('', '\xa0')
+  setFoot('\xa0/[CMD] for commands') 
+}
+
 function entryState() {
   if(localStorage.getItem('profile') !== null) {
-    //load with profile
-  } else {
-    //set login phase
+    setLogged()
+  } else {   
+    clearLines()
     setLogin()
   }
 }
 
-function setLoading(loading) {
-  if(loading) {
-    loadingState = true
+function setLoading(ldng) {
+  if(ldng) {
+    loading = true
     rec = setTimeout(() => {
       setMainCarret(`\xa0${loadChars[loadI%loadChars.length]}\xa0`)
       loadI++
       setLoading(true)
     }, 200)
   } else {
-    loadingState = false
-    clearTimeout(rec)
+    loading = false
+    if(rec !== undefined) {
+      clearTimeout(rec)
+    }
   }
 }
 
